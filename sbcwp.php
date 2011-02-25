c679d5e0238cabb53fef145e1bedb2005dd4af66
Chris Koepke <ckoepke@innovetech.com>
2011-02-25 16:29:33 -0500

    form is now output only when the sbcwp=form attribute it set in the post or get data

--- sbcwp.php
+++ sbcwp.php
@@ -8,16 +8,104 @@ Author URI: http://antennahouse.com
 License: GPL3
 */
 
-$sbcwp_form = file_get_contents('sbcwp_form');
-
 function sbcwp_form()
 {
+	$sbcwp_form = '<div class="wrap">  
 
-	return $sbcwp_form;
+	<img src="/SBC Logo.gif">
+   <h1>Document Upload</h1>
+
+	<form name="sbcwp_form">
+
+		<p>File name: <input name="file_1" type="file" size="50"></p> 
+		<p><input type="checkbox" name="omitBP" value="on" checked> Omit Blank Pages</p> 
+		<p><b>Output Format:</b></p> 
+		<p><input type="radio" name="outputType" value="PDF" checked> PDF</p> 
+		<p><input type="radio" name="outputType" value="SVG"> SVG</p> 
+		<p><input type="radio" name="outputType" value="JPEG"> JPEG</p> 
+		<p><input type="radio" name="outputType" value="PNG"> PNG</p> 
+		<p><input type="radio" name="outputType" value="XPS"> XPS</p> 
+		<p><input type="radio" name="outputType" value="INX"> INX</p> 
+		<p><input type="radio" name="outputType" value="Flash"> <font color="red"><b>Flash</b></font></p> 
+		<p><input type="radio" name="outputType" value="TIFF"> <font color="red"><b>TIFF</b></font></p> 
+		<p><input type="radio" name="outputType" value="MTIFF"> <font color="red"><b>MTIFF</b></font></p> 
+		<br> 
+		<p><b>PDF Options:</b></p>
+		<p>Version: 
+		<select name="pdfversion"> 
+			<option value="PDF1.3">PDF1.3</option> 
+			<option value="PDF1.4" selected>PDF1.4</option> 
+			<option value="PDF1.5">PDF1.5</option> 
+			<option value="PDF1.6">PDF1.6</option> 
+			<option value="PDF1.7">PDF1.7</option> 
+		</select></p> 
+		<p><input type="checkbox" name="pdfEmbedFonts" value="on"> Embed Fonts</p>
+		<br> 
+		<p><b>SVG Options:</b></p> 
+		<p>Version: 
+		<select name="svgversion"> 
+			<option value="SVG1.1" selected>SVG1.1</option> 
+			<option value="SVGBasic">SVGBasic</option> 
+			<option value="SVGTiny">SVGTiny</option> 
+		</select></p> 
+		<p><input type="checkbox" name="svgEmbedFonts" value="on"> Embed Fonts</p> 
+		<br> 
+		<p><b>JPEG/PNG Options:</b></p>
+		<p>Image DPI: 
+		<select name="rasdpi"> 
+			<option value="100">100</option> 
+			<option value="200" selected>200</option> 
+			<option value="300">300</option> 
+			<option value="400">400</option> 
+		</select></p> 
+		<p>Image Scale (percent):
+		<select name="rasscale"> 
+			<option value="25">25</option> 
+			<option value="50">50</option> 
+			<option value="75">75</option> 
+			<option value="100" selected>100</option> 
+			<option value="150">150</option> 
+			<option value="200">200</option> 
+			<option value="300">300</option> 
+		</select></p>
+		<p>Image Quality (percent):
+		<select name="rasjq"> 
+			<option value="25">25</option> 
+			<option value="50">50</option> 
+			<option value="75">75</option> 
+			<option value="90" selected>90</option> 
+			<option value="100">100</option> 
+		</select></p> 
+		<p><input type="checkbox" name="rasmono" value="on"> Monochrome</p>
+		<br>
+	</form> 
+
+		<p class="submit">
+  
+			<input type="button" name="Convert" onclick="sbcwp_form_submit()"/>
+
+		</p>
+
+	</form>
+
+</div> ';
+
+	if ( $_REQUEST['sbcwp'] == 'form' )
+	{
+
+		return $sbcwp_form;
+
+	}
+	else
+	{
+
+		return $content;
+
+	}
 
 }
 
-add_filter('the_content','sbcwp_form');
+add_action('init','sbcwp_form');
 
 function sbcwp_options()
 {
