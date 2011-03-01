c9a7422bec850061604f4642b8a75c94fc510c75
Chris Koepke <ckoepke@innovetech.com>
2011-03-01 11:36:12 -0500

    debugging postrelay.php

--- postrelay.php
+++ postrelay.php
@@ -1,9 +1,11 @@
 <?php
+//$url = 'http://rainbowpdf.no-ip.info/cgi-bin/webconverter.py';
+$url = '/wp/wp-content/plugins/SBCWP/debug.php';
 $repost = "";
 while (list($name, $value) = each($_REQUEST)) {
 $repost += $name."=".$value."&";
 }
-$ch = curl_init('http://rainbowpdf.no-ip.info/cgi-bin/webconverter.py');
+$ch = curl_init($url);
 curl_setopt ($ch, CURLOPT_POST, 1);
 curl_setopt ($ch, CURLOPT_POSTFIELDS, $repost);
 curl_exec ($ch);
