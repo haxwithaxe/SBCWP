81a5fb25296d93396a1d48f5cc9e3f3b8a10f52d
Chris Koepke <ckoepke@innovetech.com>
2011-03-13 17:54:46 -0400

    finally got the whole thing working outside the module

--- sbcwp.js
+++ sbcwp.js
@@ -1,23 +1,24 @@
-function init() {
+function sbcwp_submit() {
 
-	document.getElementById("sbcwp_form").onsubmit = function() {
+	document.getElementById("sbcwp_upload_target").onload = uploadDone;
 
-		document.getElementById("sbcwp_form").target = "sbcwp_upload_target";
+	var waitImg = '<img src="wp-content/plugins/SBCWP/wait.gif" alt="Converting your document ..."/>';
 
-		document.getElementById("sbcwp_upload_target").onload = uploadDone; //This function should be called when the iframe has compleated loading
-			// That will happen when the file is completely uploaded and the server has returned the data we need.
-
-	}
+	document.getElementById('sbcwp_message').innerHTML = waitImg;
 
 }
 
-function uploadDone() { //Function will be called when iframe is loaded
+function uploadDone() {
 
-	var ret = frames['sbcwp_upload_target'].document.getElementsByTagName('body')[0].innerHTML;
+	msgobj = top.sbcwp_upload_target.document.getElementById('sbcwp_ret');
 
-	if(ret) { //This part happens when the image gets uploaded.
+	if(!msgobj){
 
-		document.getElementById('sbcwp_message').innerHTML = ret;
+		setTimeout('uploadDone()', 10);
+
+	}else{
+
+		document.getElementById('sbcwp_message').innerHTML = msgobj.innerHTML;
 
 	}
 
--- sbcwpform.php
+++ sbcwpform.php
@@ -1,22 +1,25 @@
 <?php
 header("Content-type: text/html");
-print "testing... debuging... hard face to keyboard contact";
 $url = 'http://rainbowpdf.no-ip.info/cgi-bin/webconverter-urlonly.py';
 //$url = $_SERVER['SERVER_NAME'].'/wp/wp-content/plugins/SBCWP/debug.php';
+$MAX_WAIT = 120;
 $target_path = '/tmp/'.basename( $_FILES['file_1']['name']);
 if(move_uploaded_file($_FILES['file_1']['tmp_name'], $target_path)) {
-	$_REQUEST['file_1'] = '@'.$targetpath;
+	$_REQUEST['file_1'] = "@$target_path";
 }
 $ch = curl_init($url);
 curl_setopt ($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt ($ch, CURLOPT_POSTFIELDS, $_REQUEST);
 curl_setopt ($ch, CURLINFO_HEADER_OUT, True);
-$ret = curl_exec ($ch);
-while ( ( curl_getinfo($ch, CURLINFO_HTTP_CODE) != 200 ) && !curl_errno($ch) )
-{
-	sleep(1);
-};
+if ( $ret = curl_exec ($ch) ){
+
+	print $ret;
+
+}else{
+
+	print 'Error: '.$_SERVER['SCRIPT_FILENAME'].' '.curl_error($ch);
+
+}
 curl_close ($ch);
-print $ret;
 ?>
