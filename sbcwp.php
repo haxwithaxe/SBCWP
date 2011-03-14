3d10e34f4bd8dbd17f274ea9e05321fd402f05d2
Chris Koepke <me@haxwithaxe.net>
2011-03-14 14:18:55 -0400

    trying to move html to external file

--- sbcwp.php
+++ sbcwp.php
@@ -23,7 +23,7 @@ else
 function sbcwp_form($content)
 {
 	$SCRIPT_PATH = plugins_url('/sbcwpform.php', __FILE__);
-	$sbcwp_form_bits = file_get_contents('./sbcwpform.html');
+	$sbcwp_form_bits = file_get_contents(plugins_basename(__FILE__).'sbcwpform.html');
 	print $sbcwp_form_bits;
 	$sbcwp_form = '<script type="text/javascript" src="'.plugins_url('/sbcwp.js', __FILE__).'"></script>
 <div class="wrap">  
