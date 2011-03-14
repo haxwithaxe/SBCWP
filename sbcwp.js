8fd8c1facf1158439189e878bdb480fa94739ca2
Chris Koepke <me@haxwithaxe.net>
2011-03-14 17:39:51 -0400

    adding comments

--- sbcwp.js
+++ sbcwp.js
@@ -1,24 +1,27 @@
-function sbcwp_submit(waitPath) {
+function sbcwp_submit(waitPath) // when the SBC interface form is submitted do this stuff
+{
 
-	document.getElementById("sbcwp_upload_target").onload = uploadDone;
+	document.getElementById("sbcwp_upload_target").onload = uploadDone; // set the onload event action of the iframe to uploadDone
 
-	var waitImg = '<img src="'+waitPath+'" alt="Converting your document ..."/>';
-
-	document.getElementById('sbcwp_message').innerHTML = waitImg;
+	document.getElementById('sbcwp_message').innerHTML = '<img src="'+waitPath+'" alt="Converting your document ..."/>'; // show an animated wait gif until the iframe is done loading
 
 }
 
-function uploadDone() {
+function uploadDone() // once the iframe has loaded try to grab the response from it
+{
 
-	msgobj = top.sbcwp_upload_target.document.getElementById('sbcwp_ret');
+	msgobj = top.sbcwp_upload_target.document.getElementById('sbcwp_ret'); // try to load the contents of the iframe 
 
-	if(!msgobj){
+	if(!msgobj) // if the reponse is null then do this
+	{
 
-		setTimeout('uploadDone()', 10);
+		setTimeout('uploadDone()', 10); // wait for 10ms and try again
 
-	}else{
+	}
+	else
+	{
 
-		document.getElementById('sbcwp_message').innerHTML = msgobj.innerHTML;
+		document.getElementById('sbcwp_message').innerHTML = msgobj.innerHTML; // set the contents of the message area to the contents of the iframe
 
 	}
 
